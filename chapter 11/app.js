89a788363db63e4010a07da89986461caf8d4d5f
Shayan Ahmed <shayan.ahmed.ov@gmail.com>
2024-09-18 17:11:43 +0500

    chapter 11

---
+++ chapter 11/app.js
@@ -0,0 +1,163 @@
+// 1)
+// var cityName = prompt("Enter your CITY Name");
+// if (cityName == "Karachi") {
+//   console.log("Welcome to City of Lights");
+// } else {
+//   console.log(`Welcome to ${cityName}`);
+// }
+
+// 2)
+// var gender = prompt("Enter your Gender");
+// if (gender == "male") {
+//   console.log("Good Morning Sir");
+// } else if (gender == "female") {
+//   console.log("Good Morning Ma'am");
+// } else {
+//   console.log("Please enter correct gender");
+// }
+
+// 3)
+// var trafficSignal = prompt("Enter Traffic Signal");
+// if (trafficSignal == "red") {
+//   console.log("Must Stop");
+// } else if (trafficSignal == "yellow") {
+//   console.log("Ready to Move");
+// } else if (trafficSignal == "green") {
+//   console.log("Move Now");
+// } else {
+//   console.log("Enter Correct Traffic SIgnal");
+// }
+
+// 4)
+// var remainingFuel = prompt("Enter Remaining Fuel in Liter");
+// if (remainingFuel < 0.25) {
+//   console.log("Please refill the fuel in your Car");
+// } else {
+//   console.log("Your car has enough fuel");
+// }
+
+// 5)
+// a
+// var a = 4;
+// if (++a == 5) {
+//   alert("given condition for variable a is true");
+// }
+
+// b
+// var b = 82;
+// if (b++ === 83) {
+//   alert("Given condition for variable b is true");
+// }
+
+// c
+// var c = 12;
+
+// if (c++ === 13) {
+//   alert("condition 1 is tue");
+// }
+
+// if (c === 13) {
+//   alert("condition 2 is true");
+// }
+
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true");
+// }
+
+// d
+// var materialCost = 20000;
+// var laborCost = 2000;
+
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//   alert("The cost equals");
+// }
+
+// e
+// if (true) {
+//   alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+
+// f
+// if ("car" < "cat") {
+//   console.log("car is smaller than cat");
+// }
+
+// 6)
+// var totalMarks = prompt("Enter Total Marks");
+// var obtainedMarks = prompt("Eter your Marks");
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// if (percentage >= 80) {
+//   console.log("Grade : A-one");
+//   console.log("Remarks : Excellent");
+// } else if (percentage >= 70) {
+//   console.log("Grade : A");
+//   console.log("Remarks : Good");
+// } else if (percentage >= 60) {
+//   console.log("Grade : B");
+//   console.log("Remarks : You need to improve");
+// } else {
+//   console.log("Grade : Fail");
+//   console.log("Remarks : Sorry");
+// }
+
+// 7)
+// var secretNumber = 6;
+// var guessNumber = Number(prompt("Enter a Number"));
+// if (secretNumber === guessNumber) {
+//   console.log("Bingo! Correct Answer");
+// } else if (secretNumber === guessNumber + 1) {
+//   console.log("Close enough to the correct answer ");
+// } else {
+//   console.log("Try another number");
+// }
+
+// 8)
+// var num = Number(prompt("Enter A Number"));
+// if (num % 3 === 0) {
+//   console.log("Number is Divisible by 3");
+// } else {
+//   console.log("Number is Not Divisible by 3");
+// }
+
+// 9)
+// var num = Number(prompt("Enter A Number"));
+// if (num % 2 === 0) {
+//   console.log("Your Number is Even");
+// } else {
+//   console.log("Your Number is Odd");
+// }
+
+// 10)
+// var temp = Number(prompt("Enter Your Temperature"));
+// if (temp > 40) {
+//   console.log("It is too hot outside");
+// } else if (temp > 30) {
+//   console.log("The Weather today is Normal");
+// } else if (temp > 20) {
+//   console.log("Today's Weather is cool");
+// } else if (temp > 10) {
+//   console.log("OMG! Today's Wweather is so Cool");
+// } else {
+//   console.log("Your temperature is not correct");
+// }
+
+// 11)
+// var num1 = Number(prompt("Enter You First Number"));
+// var num2 = Number(prompt("Enter You Second Number"));
+// var operator = prompt("Enter You Operation you want to perform");
+// if ((operator === "+")) {
+//   console.log(num1 + num2);
+// } else if ((operator === "-")) {
+//   console.log(num1 - num2);
+// } else if ((operator === "*")) {
+//   console.log(num1 * num2);
+// } else if ((operator === "/")) {
+//   console.log(num1 / num2);
+// }
